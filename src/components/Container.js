77225213d3a716dfbda970a38364500ad63fc608
livotieno <livio5261@gmail.com>
2023-11-02 09:30:53 +0300

    done

--- src/components/Container.js
+++ src/components/Container.js
@@ -7,7 +7,7 @@ function Container({
   direction = "horizontal", // possible values: vertical, horizontal
   contentPosition = "center", // possible values: left, right, center
 }) {
-  return (
+  return 
     <div className={`container ${textPosition}`}>
       {header ? <h2>{header}</h2> : null}
 
