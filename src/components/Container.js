24a3f956ae4263a5b3955230df7ff35a424c5ebf
livotieno <livio5261@gmail.com>
2023-11-02 09:34:30 +0300

    add

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Container({
-  header,
-  children,
-  textPosition = "", // possible values: left, right, center
-  direction = "horizontal", // possible values: vertical, horizontal
-  contentPosition = "center", // possible values: left, right, center
-}) {
-  return 
+function Container(props) {
+  const {
+    header,
+    children,
+    textPosition = "", // possible values: left, right, center
+    direction = "horizontal", // possible values: vertical, horizontal
+    contentPosition = "center", // possible values: left, right, center
+  } = props;
+  
+  return (
     <div className={`container ${textPosition}`}>
       {header ? <h2>{header}</h2> : null}
 
